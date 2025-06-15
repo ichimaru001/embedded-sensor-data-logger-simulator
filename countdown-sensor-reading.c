312803cfed691b4e617909dd512cfe17bb281e0f
ichimaru001 <ichimarusan345@proton.me>
2025-06-15 15:08:46 +0300

    countdown and sensor timing reading prototype

--- countdown-sensor-reading.c
+++ countdown-sensor-reading.c
@@ -1,7 +1,44 @@
 #include <stdio.h>
+#include <time.h>
+#include <Windows.h>
+#include <stdint.h>
 
 int main() {
-  printf("Hello, World!");
+  clock_t start = clock();
+  uint8_t countdown = 25; // 25 seconds
+  uint8_t sensorReadingDelay = 5; // 5 seconds
+  clock_t lastSensorRead = start;
+  clock_t lastCountdownUpdate = start;
+
+
+
+
+  while (1)
+  {
+    clock_t now = clock();
+    double elapsed = (double)(now - start) / CLOCKS_PER_SEC;
+    // update last sensor read 
+    if (elapsed - lastSensorRead >= sensorReadingDelay) {
+      printf("Sensor has been read: %f seconds", elapsed - lastSensorRead);
+      lastSensorRead = elapsed;
+    }
+    // update last countdown update
+    if (elapsed - lastCountdownUpdate >= 1) {
+      countdown--;
+      printf("Countdown is now %d", countdown);
+      lastCountdownUpdate = elapsed;
+    }
+    if (countdown <= 0) {
+      break;
+    }
+  }
+
+
+
+
+  // clock_t end = clock();
+  // printf("%d", end);
+
 
   return 0;
 }
